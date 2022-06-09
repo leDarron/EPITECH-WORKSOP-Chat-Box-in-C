ed90f909f1ed664751c0adb80a4857d1404a6506
victor <victor.garreau@epitech.eu>
2022-06-09 08:46:41 -0400

    [ADD] step1

--- src/server/init_server.c
+++ src/server/init_server.c
@@ -14,10 +14,13 @@ server_t *init_server(char *ip_addr, int port)
     server->ip_addr = ip_addr;
     server->port = port;
 
-    // init socket
-    // init server addr
-    // bind
-    // listen
+    server->socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr_server;
+    addr_server.sin_family = AF_INET;
+    addr_server.sin_port = htons(port);
+    addr_server.sin_addr.s_addr = inet_addr(ip_addr);
+    bind(server->socket, (const struct sockaddr*) &addr_server, sizeof(addr_server));
+    listen(server->socket, 10);
 
     return server;
 }
